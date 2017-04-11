31bc4f6aae1132121964cf6fec50e6123f5aa4af
Lewis Kirkaldie <lewis.kirkaldie@gmail.com>
2017-04-11 18:49:35 +0100

    -fix packages.config error (should have gone) -metadata changes

--- Cinegy.TtxDecoder/Properties/AssemblyInfo.cs
+++ Cinegy.TtxDecoder/Properties/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Cinegy.TtxDecoder")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCopyright("Copyright © Cinegy GmbH 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en")]
